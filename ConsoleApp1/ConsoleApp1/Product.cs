19823f9e3af1fc6b55d1dae54e8242f9a13e18a8
friskando <119885328+friskando@users.noreply.github.com>
2026-05-17 22:48:27 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private string _category;
+        private decimal _price;
+        private int _stockQuantity;
+        private DateTime _addedDate;
+
+        // свойства для доступ
+        public int Id
+        {
+            get 
+            { 
+                return _id;
+            }
+            set 
+            {
+                _id = value; 
+            }
+        }
+
+        public string Name
+        {
+            get 
+            {
+                return _name; 
+            }
+            set 
+            { 
+                _name = value; 
+            }
+        }
+
+        public string Category
+        {
+            get 
+            { 
+                return _category; 
+            }
+            set 
+            { 
+                _category = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get 
+            { 
+                return _price;
+            }
+            set 
+            { 
+                _price = value;
+            }
+        }
+
+        public int StockQuantity
+        {
+            get 
+            { 
+                return _stockQuantity; 
+            }
+            set 
+            { 
+                _stockQuantity = value;
+            }
+        }
+
+        public DateTime AddedDate
+        {
+            get 
+            { 
+                return _addedDate;
+            }
+            set 
+            { 
+                _addedDate = value; 
+            }
+        }
+
+        internal Product()
+        {
+            _id = 0;
+            _name = "";
+            _category = "";
+            _price = 0;
+            _stockQuantity = 0;
+            _addedDate = DateTime.Now;
+        }
+
+        internal Product(int id, string name, string category,
+            decimal price, int stockQuantity)
+        {
+            _id = id;
+            _name = name;
+            _category = category;
+            _price = price;
+            _stockQuantity = stockQuantity;
+            _addedDate = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string text = $"ID:{_id}|{_name}|{_category}" +
+                $"|{_price} руб |{_stockQuantity} шт " +
+                $"| {_addedDate}";
+            return text;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string DataFile = "catalog.dat";
+        static ProductDatabase db;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("КАТАЛОГ ПРОДУКЦИИ");
+
+            db = new ProductDatabase(DataFile);
+            db.LoadFromFile();
+
+            int choice = 0;
+
+            while (choice != 5)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1 - Просмотр");
+                Console.WriteLine("2 - Добавить");
+                Console.WriteLine("3 - Удалить");
+                Console.WriteLine("4 - По категории");
+                Console.WriteLine("5 - Выход");
+                Console.Write("Выбор: ");
+
+                string input = Console.ReadLine();
+                choice = int.Parse(input);
+
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    db.ViewAllProducts();
+                }
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    AddProduct();
+                }
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    DeleteProduct();
+                }
+                else if (choice == 4)
+                {
+                    Console.Clear();
+                    ViewByCategory();
+                }
+                else if (choice == 5)
+                {
+                    db.SaveToFile();
+                    Console.WriteLine("Конец");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный выбор");
+                }
+            }
+        }
+
+        static void AddProduct()
+        {
+            Console.WriteLine("ДОБАВЛЕНИЕ ТОВАРА");
+
+            int id = db.GetNextId();
+            Console.WriteLine("Новый ID: " + id);
+
+            Console.Write("Название: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Категория: ");
+            string category = Console.ReadLine();
+
+            Console.Write("Цена: ");
+            string priceStr = Console.ReadLine();
+            decimal price = decimal.Parse(priceStr);
+
+            Console.Write("Количество: ");
+            string quantityStr = Console.ReadLine();
+            int quantity = int.Parse(quantityStr);
+
+            Product newProduct = new Product(id, name, category,
+                price, quantity);
+            db.AddProduct(newProduct);
+            db.SaveToFile();
+        }
+
+        static void DeleteProduct()
+        {
+            Console.WriteLine("УДАЛЕНИЕ ТОВАРА");
+
+            db.ViewAllProducts();
+
+            Console.Write("Введите ID для удаления: ");
+            string idStr = Console.ReadLine();
+            int id = int.Parse(idStr);
+
+            if (db.DeleteProductById(id))
+            {
+                db.SaveToFile();
+            }
+        }
+
+        static void ViewByCategory()
+        {
+            Console.WriteLine("ПРОСМОТР ПО КАТЕГОРИИ");
+
+            Console.Write("Введите категорию: ");
+            string category = Console.ReadLine();
+
+            db.ViewProductsByCategory(category);
+        }
+    }
+}
